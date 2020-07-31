7b4fd798975bd568adc87f18474d61f1d1823841
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-31 19:29:16 +0700

    Create app-bar.js

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-import main from "./src/script/view/main.js"
+import "./src/script/component/app-bar.js";
+import main from "./src/script/view/main.js";
 document.addEventListener("DOMContentLoaded", main);
