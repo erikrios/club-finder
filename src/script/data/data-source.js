504af67a6e626f97016bd64e4ef9f3406b7af538
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-12 18:45:50 +0700

    Refactor the searchClub() function

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -3,16 +3,7 @@ import clubs from "./clubs.js";
 class DataSource {
 
     static searchClub(keyword) {
-        return new Promise((resolve, reject) => {
-            const filteredClubs = clubs.filter(club =>
-                club.name.toUpperCase().includes(keyword.toUpperCase()));
-
-            if (filteredClubs.length) {
-                resolve(filteredClubs);
-            } else {
-                reject(`${keyword} is not found`);
-            }
-        });
+        return fetch(`https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=${keyword}`);
     }
 }
 
